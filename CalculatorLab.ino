2a11a9a5427b2f55520ce40f1e17dd6f54ef2771
lpuatu <54289824+lpuatu@users.noreply.github.com>
2021-11-17 10:21:33 -0600

    Add files via upload

---
+++ CalculatorLab.ino
@@ -0,0 +1,18 @@
+#include "cowpi.h"
+
+char keys[4][4] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

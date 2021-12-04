0b81651c41ecd8b86d73005926f5184520a1cc88
Ryan Brown <rbrown53@huskers.unl.edu>
2021-12-03 21:07:16 -0600

    Bug Fix

--- CalculatorLab.ino
+++ CalculatorLab.ino
@@ -13,12 +13,6 @@ int operandPosition = 1;
 uint8_t lastOperation = 0x0E;
 uint8_t get_key_pressed();
 
-void handle_buttonpress();
-void handle_keypress();
-
-volatile uint8_t last_key_pressed = 244;
-volatile unsigned long last_time_keypad_pressed = 0xFFFFFFFF;
-
 struct gpio_registers *gpio;
 struct spi_registers *spi;
 unsigned long last_keypad_press = 0;
@@ -44,8 +38,6 @@ void setup() {
   gpio = gpio = (gpio_registers *)(IObase+0x03);
   spi = (spi_registers *)(IObase+0x2C);
 
-  attachInterrupt(digitalPinToInterrupt (2), handle_buttonpress , CHANGE );
-  attachInterrupt(digitalPinToInterrupt (3), handle_keypress , CHANGE );
 
   setup_simple_io();
   setup_keypad();
@@ -58,7 +50,6 @@ void loop() {
   uint8_t left_button_current_position = gpio[D8_D13].input & (1);
   unsigned long now = millis();
 
-<<<<<<< HEAD
      if ((~(gpio[A0_A5].input)& 0b1111)>0  && (millis() - last_keypad_press > 500)) {
         //keyPressed(get_key_pressed());
         inputDisplay(get_key_pressed());
@@ -69,26 +60,6 @@ void loop() {
        gpio[D8_D13].output |= 1<<4;
      }
 
-=======
-  if(now - last_time_keypad_pressed > 100 && (last_key_pressed >= 0 && last_key_pressed < 16)){
-    Serial.println("Keypad pressed");
-    inputDisplay(last_key_pressed);
-    last_key_pressed = 244;
-    last_time_keypad_pressed = 0xFFFFFFFF;
-  }
-
-    //  if ((~(gpio[A0_A5].input)& 0b1111)>0  && (millis() - last_keypad_press > 500)) {
-    //     //keyPressed(get_key_pressed());
-    //     inputDisplay(get_key_pressed());
-    //  }
-
-    //  if(millis() - last_keypad_press > 500){
-    //   gpio[D8_D13].output &= ~(1<<4);
-    //  }else{
-    //    gpio[D8_D13].output |= 1<<4;
-    //  }
-
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
     if (!left_button_current_position && (now - last_left_button_press > 500)) {
     leftButtonPressed();
     last_left_button_press = now;
@@ -101,150 +72,9 @@ void loop() {
 }
 
 void inputDisplay(uint8_t key){
-<<<<<<< HEAD
   keyPressed(key);
-=======
-  //Display the number!
-  if(operand2 * 10 < 99999999){
-  if(key == 0x01){
-    operand2 = (operand2 * 10L)+1L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x02){
-    operand2 = (operand2 * 10L)+2L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x03){
-    operand2 = (operand2 * 10L)+3L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x04){
-    operand2 = (operand2 * 10L)+4L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x05){
-    operand2 = (operand2 * 10L)+5L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x06){
-    operand2 = (operand2 * 10L)+6L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x07){
-    operand2 = (operand2 * 10L)+7L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x08){
-    operand2 = (operand2 * 10L)+8L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x09){
-    operand2 = (operand2 * 10L)+9L;
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  if(key == 0x00){
-    operand2 = (operand2 * 10L);
-    operandPosition = 2;
-    operand2Null = 0;
-    Serial.println(operand2);
-  }
-  displayCurrentNumber();
-}
-  //End of number display
-  if(key == 0x0A || 0x0B || 0x0C || 0x0D || 0x0E){
-    calculate(key);
-  }
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
 }
 
-void calculate(uint8_t key){
-  if(key == 0x0A){
-    operand1 = operand2;
-    operand2 = 0;
-    operand2Null = 1;
-    lastOperation = 0x0A;
-    Serial.println("Addition");
-  }
-  if(key == 0x0B){
-    operand1 = operand2;
-    operand2 = 0;
-    operand2Null = 1;
-    lastOperation = 0x0B;
-    Serial.println("Subtraction");
-  }
-  if(key == 0x0C){
-    operand1 = operand2;
-    operand2 = 0;
-    operand2Null = 1;
-    lastOperation = 0x0C;
-    Serial.println("Multiplication");
-  }
-  if(key == 0x0D){
-    operand1 = operand2;
-    operand2 = 0;
-    operand2Null = 1;
-    lastOperation = 0x0D;
-    Serial.println("Division");
-  }
-  if(key == 0x0E){
-    if(lastOperation == 0x0A){
-      lastOperand = operand2;
-      operand2 = operand1 + operand2;
-      operand1 = 0;
-      operandPosition = 2;
-      operand2Null = 0;
-      Serial.println(operand2);
-    }
-    if(lastOperation == 0x0B){
-      lastOperand = operand2;
-      operand2 = operand1 - operand2;
-      operand1 = 0;
-      operandPosition = 2;
-      operand2Null = 0;
-      Serial.println(operand2);
-    }
-    if(lastOperation == 0x0C){
-      lastOperand = operand2;
-      operand2 = operand1 * operand2;
-      operand1 = 0;
-      operandPosition = 2;
-      operand2Null = 0;
-      Serial.println(operand2);
-    }
-    if(lastOperation == 0x0D){
-      lastOperand = operand2;
-      operand2 = operand1 / operand2;
-      operand1 = 0;
-      operandPosition = 2;
-      operand2Null = 0;
-      Serial.println(operand2);
-    }
-    if(lastOperation == 0x0E){
-      calculate(key);
-    }
-    Serial.println("Equal to");
-    displayCurrentNumber();
-    lastOperation = 0x0E;
-  }
-}
 
 void setup_simple_io() {
   gpio[A0_A5].direction &= 0b11001111;
@@ -275,10 +105,7 @@ void setup_display_module() {
   for (char i = 1; i <= 8; i++) {
     display_data(i, 0);     // clear all digit registers
   }
-<<<<<<< HEAD
   display_data(1,seven_segments[0]);
-=======
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
 }
 
 uint8_t get_key_pressed() {
@@ -336,26 +163,37 @@ void rightButtonPressed(){
 void keyPressed(uint8_t key){
   int shouldDisplayError = 0;
   Serial.println(key, HEX);
-
   if(key <=0x09){
     if(operandPosition ==1){
+      Serial.println(operand2, HEX);
       if(operand1Null == 1){
         operand1=0;
         operand1Null=0;
       }
       if(abs(operand1)/ 1000000 <1){
         operand1 *=10;
-        operand1 += key;
+        if(operand1 >=0){
+          operand1 += key;
+        }else{
+          operand1 -= key;
+        }
+        Serial.println(operand1, HEX);
       }
     }
     if(operandPosition ==2){
+      Serial.println(operand2, HEX);
       if(operand2Null == 1){
         operand2=0;
         operand2Null=0;
       }
       if(abs(operand2) / 1000000 <1){
         operand2 *=10;
-        operand2 += key;
+        if(operand2 >=0){
+          operand2 += key;
+        }else{
+          operand2 -= key;
+        }
+        Serial.println(operand2, HEX);
       }
     }
   }
@@ -419,10 +257,7 @@ void displayCurrentNumber(){
       for (char i = 1; i <= 8; i++) {
         display_data(i, 0);
       }
-<<<<<<< HEAD
       display_data(1,seven_segments[0]);
-=======
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
     }else{
       long intToDisplay = operand1;
       int indexInDisplay = 1;
@@ -449,10 +284,7 @@ void displayCurrentNumber(){
       for (char i = 1; i <= 8; i++) {
         display_data(i, 0);
       }
-<<<<<<< HEAD
        display_data(1,seven_segments[0]);
-=======
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
     }else{
       long intToDisplay = operand2;
       int indexInDisplay = 1;
@@ -501,16 +333,4 @@ void displayError(){
   display_data(3,0b101);
   display_data(4,0b101);
   display_data(5,0b1001111);
-<<<<<<< HEAD
-=======
-}
-
-void handle_keypress(){
-  uint8_t key_pressed = get_key_pressed();
-
-  if(last_key_pressed != key_pressed){
-    last_key_pressed = key_pressed;
-    last_time_keypad_pressed = millis();
-  }
->>>>>>> fe4547d07f31f74ec76b179463edb3716b6fc3fd
 }
